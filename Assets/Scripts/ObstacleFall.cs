c9cbf712a7e5704792e19713a9f33e283832aba2
Heisenberg <66485604+NR-NJN@users.noreply.github.com>
2024-04-04 16:56:30 +0530

    Destroy cubes after reaching out of bounds, and added UI elements

--- Assets/Scripts/ObstacleFall.cs
+++ Assets/Scripts/ObstacleFall.cs
@@ -7,12 +7,20 @@ public class ObstacleFall : MonoBehaviour
 {
     public Vector2 fallSpeed;
     float speed;
+
+    float heightThreshold;
     void Start()
     {
+        heightThreshold = -Camera.main.orthographicSize-transform.localScale.y;
         speed = Mathf.Lerp(fallSpeed.x, fallSpeed.y, Settings.GetMaxDifficulty());
     }
     void Update()
     {
         transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        if (transform.position.y<heightThreshold)
+        {
+            Destroy(gameObject);
+        }
     }
 }
