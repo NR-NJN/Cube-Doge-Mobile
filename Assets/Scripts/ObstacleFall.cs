c6183aab26b54a128ece5918d1c6a5e52107b7ab
Heisenberg <66485604+NR-NJN@users.noreply.github.com>
2024-04-04 15:58:08 +0530

    Set fall speed to linearly interpolate with the cube count

--- Assets/Scripts/ObstacleFall.cs
+++ Assets/Scripts/ObstacleFall.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D;
 using UnityEngine;
 
 public class ObstacleFall : MonoBehaviour
 {
     public Vector2 fallSpeed;
-    private void Start()
+    float speed;
+    void Start()
     {
-        
+        speed = Mathf.Lerp(fallSpeed.x, fallSpeed.y, Settings.GetMaxDifficulty());
     }
     void Update()
     {
-        transform.Translate(Vector2.down * fallSpeed * Time.deltaTime);
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
     }
 }
